74275b5e46c2a2615f592ecd3055074ec3337383
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-12-21 21:16:46 +0000

    tried fixing issue

--- index.js
+++ index.js
@@ -29,42 +29,78 @@ const addDepartment = [
     }
 ]
 
-function init() {
-    inquirer.prompt(questions).then((answers) => {
+async function viewAllDepartments() {
+  db.query('SELECT * FROM department;', function (err, results) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(results);
+  });
+  return;
+}
+
+async function viewAllRoles() {
+  db.query('SELECT title AS job_title, role.id AS role_id, department.name AS department, salary FROM department JOIN role ON role.department_id = department.id;', function (err, results) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(results);
+  });
+  return;
+}
+
+async function viewAllEmployees() {
+  db.query('SELECT employee.id, employee.first_name, employee.last_name , role.title, department.name, role.salary, employee.manager_id FROM department JOIN role ON role.department_id = department.id JOIN employee ON employee.role_id = role.id;', async function (err, results) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(results);
+  });
+  return;
+}
+
+async function init() {
+    inquirer.prompt(questions).then(async (answers) => {
         switch(answers.options){
           case 'view all departments':
-            db.query('SELECT * FROM department;', function (err, results) {
-                console.log(results);
-            });
+            await viewAllDepartments();
+            return true
             break;
           case 'view all roles':
-            db.query('SELECT title AS job_title, role.id AS role_id, department.name AS department, salary FROM department JOIN role ON role.department_id = department.id;', function (err, results) {
-                console.log(results);
-            });
+            await viewAllRoles();
+            return true
             break;
           case 'view all employees':
-            db.query('SELECT employee.id, employee.first_name, employee.last_name , role.title, department.name, role.salary, employee.manager_id FROM department JOIN role ON role.department_id = department.id JOIN employee ON employee.role_id = role.id;', function (err, results) {
-                console.log(results);
-            });
+            await viewAllEmployees();
+            return true
             break;
           case 'add a department':
-            addNewDepartment();
-          default:
-            break
+            await addNewDepartment();
+            return true
+            break;
+          case 'quit':
+            break;
         }
-        if(answers.options != 'quit'){
-            init()
-        };
-  
-    }).then((res) => console.log("goodbye"))
+
+    }).then((res) => {
+      if(res){
+        init()
+      }else{
+        console.log("goodbye");
+        db.end();
+      }
+    })
   }
 
-  function addNewDepartment(){
+  async function addNewDepartment(){
     inquirer.prompt(addDepartment).then((answers) => {
         db.query('INSERT INTO department (name) VALUES ("?")', answers.newDepartment, function (err, results) {
             console.log('Added new department');
         });
-    }).then((res) => null)
+    })
   }
 
   init();
