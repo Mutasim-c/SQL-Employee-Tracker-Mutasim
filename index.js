4246524464074bda1796892c32006426b7a14655
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-12-21 22:32:52 +0000

    got files to run smoothly

--- index.js
+++ index.js
@@ -29,78 +29,135 @@ const addDepartment = [
     }
 ]
 
-async function viewAllDepartments() {
-  db.query('SELECT * FROM department;', function (err, results) {
+// async function viewAllDepartments() {
+//   db.query('SELECT * FROM department;', function (err, results) {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(results);
+//   })
+//   init();
+// }
+
+let viewAllDepartments = async () => {
+  const myQuery = "SELECT * FROM department;";
+
+  // getting the result of the query
+  let results = await new Promise((resolve, reject) => db.query(myQuery, (err, results) => {
     if (err) {
-      console.error(err);
-      return;
+      reject(err)
+    } else {
+      console.log(results);
+      resolve(results);
     }
-    console.log(results);
-  });
-  return;
+  }));
+  console.log("query ready");
+  // call bar and waiting the result
+  init()
 }
 
-async function viewAllRoles() {
-  db.query('SELECT title AS job_title, role.id AS role_id, department.name AS department, salary FROM department JOIN role ON role.department_id = department.id;', function (err, results) {
+// async function viewAllRoles() {
+//   db.query('SELECT title AS job_title, role.id AS role_id, department.name AS department, salary FROM department JOIN role ON role.department_id = department.id;', function (err, results) {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(results);
+//   }).then(init());
+// }
+let viewAllRoles = async () => {
+  const myQuery = "SELECT title AS job_title, role.id AS role_id, department.name AS department, salary FROM department JOIN role ON role.department_id = department.id;";
+
+  // getting the result of the query
+  let results = await new Promise((resolve, reject) => db.query(myQuery, (err, results) => {
     if (err) {
-      console.error(err);
-      return;
+      reject(err)
+    } else {
+      console.log(results);
+      resolve(results);
     }
-    console.log(results);
-  });
-  return;
+  }));
+  console.log("query ready");
+  // call bar and waiting the result
+  init()
 }
 
-async function viewAllEmployees() {
-  db.query('SELECT employee.id, employee.first_name, employee.last_name , role.title, department.name, role.salary, employee.manager_id FROM department JOIN role ON role.department_id = department.id JOIN employee ON employee.role_id = role.id;', async function (err, results) {
+// async function viewAllEmployees() {
+//   db.query('SELECT employee.id, employee.first_name, employee.last_name , role.title, department.name, role.salary, employee.manager_id FROM department JOIN role ON role.department_id = department.id JOIN employee ON employee.role_id = role.id;', async function (err, results) {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(results);
+//   }).then(init());
+// }
+let viewAllEmployees = async () => {
+  const myQuery = "SELECT employee.id, employee.first_name, employee.last_name , role.title, department.name, role.salary, employee.manager_id FROM department JOIN role ON role.department_id = department.id JOIN employee ON employee.role_id = role.id;";
+
+  // getting the result of the query
+  let results = await new Promise((resolve, reject) => db.query(myQuery, (err, results) => {
     if (err) {
-      console.error(err);
-      return;
+      reject(err)
+    } else {
+      console.log(results);
+      resolve(results);
     }
-    console.log(results);
-  });
-  return;
+  }));
+  console.log("query ready");
+  // call bar and waiting the result
+  init()
 }
+
 
 async function init() {
     inquirer.prompt(questions).then(async (answers) => {
         switch(answers.options){
           case 'view all departments':
             await viewAllDepartments();
-            return true
             break;
           case 'view all roles':
             await viewAllRoles();
-            return true
             break;
           case 'view all employees':
             await viewAllEmployees();
-            return true
             break;
           case 'add a department':
             await addNewDepartment();
-            return true
             break;
           case 'quit':
+            db.end();
             break;
         }
 
-    }).then((res) => {
-      if(res){
-        init()
-      }else{
-        console.log("goodbye");
-        db.end();
-      }
     })
   }
 
-  async function addNewDepartment(){
-    inquirer.prompt(addDepartment).then((answers) => {
-        db.query('INSERT INTO department (name) VALUES ("?")', answers.newDepartment, function (err, results) {
-            console.log('Added new department');
-        });
-    })
+  // async function addNewDepartment(){
+  //   inquirer.prompt(addDepartment).then((answers) => {
+  //       db.query('INSERT INTO department (name) VALUES ("?")', answers.newDepartment, function (err, results) {
+  //           console.log('Added new department');
+  //       });
+  //   })
+    
+  // }
+
+  let addNewDepartment = async () => {
+    const myQuery = 'INSERT INTO department (name) VALUES ("?")';
+  
+    // getting the result of the query
+    inquirer.prompt(addDepartment).then(async (answers) => {
+    let results = await new Promise((resolve, reject) => db.query(myQuery,answers.newDepartment, (err, results) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(results);
+      }
+    }));
+    console.log("query ready");
+    // call bar and waiting the result
+    init()
+  })
   }
 
   init();
